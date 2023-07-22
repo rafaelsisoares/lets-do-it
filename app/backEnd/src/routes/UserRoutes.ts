ef26b2e8f441e0d239519114c32d88634bf6fce9
Rafael Soares <r.soares2015@outlook.com>
2023-07-22 00:48:35 -0300

    feat: Criacao da rota de login

--- app/backEnd/src/routes/UserRoutes.ts
+++ app/backEnd/src/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import checkLoginData from "../middlewares/checkLoginData";
 import UserController from "../controllers/UserController";
 
 const controller = new UserController();
 const router = Router();
 
 router.post('/', (req, res) => controller.createUser(req, res));
+router.get('/login', checkLoginData, (req, res) => controller.login(req, res));
 
 export default router;
