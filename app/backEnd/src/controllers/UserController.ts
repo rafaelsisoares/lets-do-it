77c14ee43628dfd2048865bb6b0d569beb25e075
Rafael Soares <r.soares2015@outlook.com>
2023-07-26 20:21:42 -0300

    feat: Criacao do metodo que retorna um usuario por meio de token controller

--- app/backEnd/src/controllers/UserController.ts
+++ app/backEnd/src/controllers/UserController.ts
@@ -13,4 +13,10 @@ export default class UserController {
         const { cod, message } = await this._service.login(req.body);
         return res.status(cod).json({ message });
     }
+
+    async getUserByToken(req: Request, res: Response): Promise<Response> {
+        const { authorization } = req.headers;
+        const { cod, message } = await this._service.getUserByToken(authorization);
+        return res.status(cod).json({ message: JSON.parse(message) })
+    }
 }
