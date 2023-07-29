33f063343a10dec14fe68f63b4120af6639681e8
Rafael Soares <r.soares2015@outlook.com>
2023-07-29 00:12:03 -0300

    feat: Criacao do metodo de remocao de usuario controller

--- app/backEnd/src/controllers/UserController.ts
+++ app/backEnd/src/controllers/UserController.ts
@@ -25,4 +25,14 @@ export default class UserController {
         const { cod, message } = await this._service.updateUser(req.body, +id);
         return res.status(cod).json({ message })
     }
+
+    async removeUser(req: Request, res: Response): Promise<Response> {
+        const { authorization } = req.headers;
+        const { id } = req.params;
+        if (!authorization) {
+            return res.status(401).json({ message: 'Token not found' });
+        }
+        const { cod, message } = await this._service.removeUser(authorization, +id);
+        return res.status(cod).json({ message });
+    }
 }
