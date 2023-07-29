e160cc436c302af400c5ab9389755b6339ef311c
Rafael Soares <r.soares2015@outlook.com>
2023-07-29 00:17:10 -0300

    feat: Criacao da rota de remocao de usuario

--- app/backEnd/src/routes/UserRoutes.ts
+++ app/backEnd/src/routes/UserRoutes.ts
@@ -11,5 +11,6 @@ router.post('/', checkNewUserData, (req, res) => controller.createUser(req, res)
 router.get('/login', checkLoginData, (req, res) => controller.login(req, res));
 router.get('/', checkToken,  (req, res) => controller.getUserByToken(req, res));
 router.put('/:id', checkToken, checkNewUserData, (req, res) => controller.updateUser(req, res));
+router.delete('/:id', checkToken, (req, res) => controller.removeUser(req, res));
 
 export default router;
