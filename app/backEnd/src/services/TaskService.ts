56ba7040b0f28d4f3e87fdca495059db1b74217d
Rafael Soares <r.soares2015@outlook.com>
2023-08-01 00:22:24 -0300

    feat: Criacao do service de tasks

---
+++ app/backEnd/src/services/TaskService.ts
@@ -0,0 +1,20 @@
+import { ModelStatic } from "sequelize";
+import TaskModel from "../database/models/TaskModel";
+import { IResponse, ITask } from "src/interfaces";
+
+export default class TaskService {
+    private _model: ModelStatic<TaskModel> = TaskModel;
+
+    async createTask(task: ITask): Promise<IResponse> {
+        try {
+            const { dataValues } = await this._model.create({ ...task });
+            return {
+                cod: 201,
+                message: `Task ${dataValues.task} created!`,
+            }
+        } catch (e) {
+            console.error(e);
+            return { cod: 400, message: 'Error' }
+        }
+    }
+}
