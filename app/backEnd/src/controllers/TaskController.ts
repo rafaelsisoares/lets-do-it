69678dc13c2ac45dfbe260cf22ba9c3a29a6d87b
Rafael Soares <r.soares2015@outlook.com>
2023-08-01 00:27:27 -0300

    feat: Criacao do controller de tasks

---
+++ app/backEnd/src/controllers/TaskController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import TaskService from "src/services/TaskService";
+
+export default class TaskController {
+    private _service = new TaskService();
+
+    async createTask(req: Request, res: Response): Promise<Response> {
+        const { cod, message } = await this._service.createTask(req.body);
+        return res.status(cod).json({ message });
+    }
+}
