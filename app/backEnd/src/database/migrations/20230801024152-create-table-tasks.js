32552ff48e6820225aaf0e96545a1fab69de7a0e
Rafael Soares <r.soares2015@outlook.com>
2023-07-31 23:50:15 -0300

    feat: Criacao da migration da tabela tasks

---
+++ app/backEnd/src/database/migrations/20230801024152-create-table-tasks.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', { 
+      id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      task: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      completed: Sequelize.BOOLEAN,
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
