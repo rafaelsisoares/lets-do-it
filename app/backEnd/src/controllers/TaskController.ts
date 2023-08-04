c577a0d494522132179b48566140e70f62876476
Rafael Soares <r.soares2015@outlook.com>
2023-08-03 23:50:04 -0300

    feat: Criacao do metodo de atualizacao de tarefas no controller

--- app/backEnd/src/controllers/TaskController.ts
+++ app/backEnd/src/controllers/TaskController.ts
@@ -20,4 +20,11 @@ export default class TaskController {
         const { cod, message } = await this._service.updateCompleted(+id)
         return res.status(cod).json({ message });
     }
+
+    async updateTask(req: Request, res: Response): Promise<Response> {
+        const { authorization } = req.headers;
+        const { id } = req.params;
+        const { cod, message } = await this._service.updateTask(+id, authorization, req.body);
+        return res.status(cod).json({ message });
+    }
 }
